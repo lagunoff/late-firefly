cff09d7ef6b3ecb04a510f1047122c609c8b9860
Vladislav <vlad.lagunoff@gmail.com>
2020-06-28 06:28:30 +0400

    918e60bf-9b5c-4c95-83c8-27e324d7ce2e

--- build-docker-action/entrypoint.sh
+++ build-docker-action/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-time=$(ghc --version)
-echo "::set-output name=time::$time"
+# disable the keychain credential helper
+git config --global credential.helper ""
+# enable the local store credential helper
+git config --global --add credential.helper store
+# add credential
+echo "https://x-access-token:${1}@github.com" >> ~/.git-credentials
+# tell git to use https instead of ssh whenever it encounters it
+git config --global url."https://github.com/".insteadof git@github.com:
 
 nix-build -A ghc.late-firefly -o result
